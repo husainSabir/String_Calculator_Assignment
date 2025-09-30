8ea9bf0d79d06552c2468c3cbff361fbb360b26d
husainSabir <2019267@iiitdmj.ac.in>
2025-09-30 20:05:37 +0530

    Create StringCalculator Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import StringCalculatorComponent from './stringCalculatorComponent';
 
 function App() {
   return (
     <div>
-      <>Hello World</>
+      <StringCalculatorComponent/>
     </div>
   );
 }
