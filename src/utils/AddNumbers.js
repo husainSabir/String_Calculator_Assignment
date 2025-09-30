bdb0fbed5ab2815e291f4656f96021277d85d3d4
husainSabir <2019267@iiitdmj.ac.in>
2025-09-30 21:56:01 +0530

    Fixed logic for negative numbers

--- src/utils/AddNumbers.js
+++ src/utils/AddNumbers.js
@@ -6,7 +6,7 @@ function AddNumbers(inputNumbers){
     if(numberList.length === 0){
         return 0;
     }
-    const negativeNumbers = numberList.filter(n => n < 1000);
+    const negativeNumbers = numberList.filter(n => n < 0);
     if (negativeNumbers.length) {
         throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
     }
